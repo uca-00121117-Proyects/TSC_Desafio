de9820aad4988474d896c4ab34978c558440eeee
Drubico <00121117@uca.edu.sv>
2020-06-22 02:42:34 -0600

    Comenzando de nuevo con Java ahora

--- paths.py
+++
@@ -1,8 +0,0 @@
-import os
-# aqui estan las rutas
-dirname = os.path.dirname(__file__)
-path_dogs1 = os.path.join(dirname, 'src/images/dog/d1.gif')
-path_dogs2 = os.path.join(dirname, 'src/images/dog/d2.gif')
-path_dogs3 = os.path.join(dirname, 'src/images/dog/d3.gif')
-path_dogs4 = os.path.join(dirname, 'src/images/dog/d4.gif')
-path_dogs5 = os.path.join(dirname, 'src/images/dog/d5.gif')
